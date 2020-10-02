a34fd01ef9afcd4de82e06cbe13553979bacd491
VolDam256 <63514298+VolDam256@users.noreply.github.com>
2020-10-02 19:07:16 +0000

    Deploying to gh-pages from  @ 79258dc8848bf649c7255465d564569cd390ffcf 🚀

---
+++ precache-manifest.8279bfe37aeb6971cbda3b52e51cc0c7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f05bbc0aeda0f1a46bbc1da55425a9f",
+    "url": "/MSTREACT/index.html"
+  },
+  {
+    "revision": "f7bb7c7b4e5ec3586d2e",
+    "url": "/MSTREACT/static/css/main.c1839c9b.chunk.css"
+  },
+  {
+    "revision": "2050e836c563ff28f38c",
+    "url": "/MSTREACT/static/js/2.b36c905a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MSTREACT/static/js/2.b36c905a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bb7c7b4e5ec3586d2e",
+    "url": "/MSTREACT/static/js/main.59a17d44.chunk.js"
+  },
+  {
+    "revision": "84fc08c6f8067b8d1a73",
+    "url": "/MSTREACT/static/js/runtime-main.8a8b3e04.js"
+  },
+  {
+    "revision": "e903dd6537f8d86d025988eade5324d3",
+    "url": "/MSTREACT/static/media/Gilroy-ExtraBold.e903dd65.woff2"
+  },
+  {
+    "revision": "64488e9410cd0a9cda06c8582c0d182b",
+    "url": "/MSTREACT/static/media/Gilroy-Light.64488e94.woff2"
+  },
+  {
+    "revision": "2222f1fd23aa2c08af158311d680ac4a",
+    "url": "/MSTREACT/static/media/Roboto-Regular.2222f1fd.woff2"
+  },
+  {
+    "revision": "2af1fd0925e5bbf65f3482826c17f580",
+    "url": "/MSTREACT/static/media/pic0.2af1fd09.png"
+  },
+  {
+    "revision": "bb598cd55152c3770000f63aae8a5747",
+    "url": "/MSTREACT/static/media/pic1.bb598cd5.png"
+  },
+  {
+    "revision": "573d0559da2e23131f510cdbe4792d01",
+    "url": "/MSTREACT/static/media/pic2.573d0559.png"
+  },
+  {
+    "revision": "ce12f7149a32113f14bbd79994eb481f",
+    "url": "/MSTREACT/static/media/pic3.ce12f714.png"
+  },
+  {
+    "revision": "6c427c5a12b68e1c2784e20eeca6825d",
+    "url": "/MSTREACT/static/media/pic4.6c427c5a.png"
+  },
+  {
+    "revision": "a5c2ded47f769b3e4d310554404db3cc",
+    "url": "/MSTREACT/static/media/pic5.a5c2ded4.png"
+  }
+]);
